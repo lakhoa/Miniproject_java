0b6cb7a5ac86ab21c1f0406e8f0065d6d3dbff1f
Le <anhkhoacla@gmail.com>
2022-06-06 10:50:33 +0700

    New

---
+++ src/test/java/com/example/MiniProject_java/MiniProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniProject_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
