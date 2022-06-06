0893897b1adf651d2fd77507fe2809b592abc74b
lakhoa <88267101+lakhoa@users.noreply.github.com>
2022-06-06 10:40:35 +0700

    Add files via upload

---
+++ MiniProject_java/src/main/java/com/example/MiniProject_java/MiniProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniProject_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectJavaApplication.class, args);
+	}
+
+}
