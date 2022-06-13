a779547bc838157bd92706ee47d53dcb4fac6267
lakhoa <88267101+lakhoa@users.noreply.github.com>
2022-06-13 14:14:57 +0700

    Add files via upload

---
+++ src/main/java/com/example/Miniproject_spring/MiniprojectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Miniproject_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Miniproject_spring/MiniprojectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Miniproject_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
